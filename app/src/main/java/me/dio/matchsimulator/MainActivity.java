94a42bbeeb4afa6340f9b9e7a3661be5f3b105c9
HenriqueAzT <henriqueazara3311@gmail.com>
2022-07-22 11:09:56 -0300

    Material Design

---
+++ app/src/main/java/me/dio/matchsimulator/MainActivity.java
@@ -0,0 +1,19 @@
+package me.dio.matchsimulator;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import me.dio.matchsimulator.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+}
